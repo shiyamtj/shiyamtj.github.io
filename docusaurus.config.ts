9a6c504cf71e6d802935e3b33a8689efb9c72e91
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-03 08:25:30 +1300

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,6 +138,8 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  plugins: [require.resolve('docusaurus-lunr-search')],
 }
 
 export default config
